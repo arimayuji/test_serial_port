597f9b16d6e0b8a8c54247a64b4e474958e55b89
Yuji Arima <yujiarima@hotmail.com>
2024-09-18 19:35:24 -0300

    chore: adds childroutes +  lazySingletons

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:test_for_serial_port/app/controller/provider/serial_port_provider.dart';
+import 'package:test_for_serial_port/app/services/serial_reader_service.dart';
+import 'package:test_for_serial_port/app/services/serial_service.dart';
+import 'package:test_for_serial_port/app/views/home/charts_page.dart';
+import 'package:test_for_serial_port/app/views/home/history_page.dart';
 import 'package:test_for_serial_port/app/views/home/home_page.dart';
 import 'package:test_for_serial_port/app/views/landing/landing_page.dart';
 import 'package:test_for_serial_port/app/views/landing/splash_page.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+  }
 
   @override
   void routes(r) {
@@ -16,7 +22,11 @@ class AppModule extends Module {
 
 class HomeModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addSingleton<SerialPortProvider>(SerialPortProvider.new);
+    i.addLazySingleton<SerialPortService>(SerialPortService.new);
+    i.addLazySingleton<SerialPortReaderService>(SerialPortReaderService.new);
+  }
 
   @override
   void routes(r) {
@@ -26,6 +36,14 @@ class HomeModule extends Module {
           ChildRoute(
             '/home',
             child: (context) => const HomePage(),
+          ),
+          ChildRoute(
+            '/charts',
+            child: (context) => const ChartsPage(),
+          ),
+          ChildRoute(
+            '/history',
+            child: (context) => const HistoryPage(),
           ),
         ]);
   }
