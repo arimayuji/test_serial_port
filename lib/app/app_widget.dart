41a46595e5f02313a70cd27cdb938b5f0aba6d7d
Yuji Arima <yujiarima@hotmail.com>
2024-09-18 13:18:39 -0300

    feat: :sparkles: adds app_widget

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:test_for_serial_port/app/shared/helpers/functions/global_snackbar.dart';
+import 'package:test_for_serial_port/app/shared/themes/app_colors.dart';
+import 'package:test_for_serial_port/app/shared/themes/app_themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: AppColors.primary,
+        systemNavigationBarColor: AppColors.primary,
+      ),
+    );
+    return MaterialApp.router(
+      scaffoldMessengerKey: rootScaffoldMessengerKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Formulários',
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+      theme: AppThemes.lightTheme,
+      themeMode: ThemeMode.system,
+    );
+  }
+}
