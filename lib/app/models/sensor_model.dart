ee45299e8fd17b2db06f32c49a89b39eee72f6ec
Yuji Arima <yujiarima@hotmail.com>
2024-09-22 22:33:31 -0300

    refactor: :recycle: updates sensor model

--- lib/app/models/sensor_model.dart
+++ lib/app/models/sensor_model.dart
@@ -1,11 +1,8 @@
-import 'package:test_for_serial_port/app/models/meter_model.dart';
+import 'package:test_for_serial_port/app/models/measurement_history_model.dart';
 
 class SensorModel {
   final String id;
-  final MeterModel meter_0;
-  final MeterModel meter_1;
+  List<MeasurementHistory> history = [];
 
-  SensorModel({required this.id})
-      : meter_0 = MeterModel(sensorId: id), 
-        meter_1 = MeterModel(sensorId: id); 
+  SensorModel({required this.id, required this.history});
 }
