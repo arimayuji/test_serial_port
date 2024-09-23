ad7c545b1b70f62e4577c8397d46582fde1dcdaa
Yuji Arima <yujiarima@hotmail.com>
2024-09-22 22:29:50 -0300

    chore: :lipstick: updates colors + icons of application

--- lib/app/shared/themes/app_colors.dart
+++ lib/app/shared/themes/app_colors.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color get primary => const Color.fromARGB(255, 191, 52, 1);
+  static Color get primary => const Color.fromARGB(255, 239, 191, 33);
   static Color get secondary => Colors.white;
   static Color get secondaryGrey => Colors.grey;
+
   static Color get error => Colors.redAccent;
+  static Color get success => Colors.greenAccent;
+
+  static Color get blue => Colors.blueAccent;
+  static Color get orange => Colors.orangeAccent;
 }
--- lib/app/shared/themes/app_icons.dart
+++ lib/app/shared/themes/app_icons.dart
@@ -3,49 +3,73 @@ import 'package:test_for_serial_port/app/shared/themes/app_colors.dart';
 import 'package:test_for_serial_port/app/shared/themes/app_dimensions.dart';
 
 class AppIcons {
-  static Icon connectIcon =  Icon(
-    Icons.outlet_outlined,
+  static Icon errorIcon = Icon(
+    Icons.error,
     size: 200,
     color: AppColors.primary,
   );
 
-  static Icon barChartIcon =  Icon(
+  static Icon connectIcon = Icon(
+    Icons.usb,
+    size: 200,
+    color: AppColors.primary,
+  );
+
+  static Icon noConnectIcon = Icon(
+    Icons.usb_off,
+    size: 200,
+    color: AppColors.primary,
+  );
+
+  static Icon barChartIcon = Icon(
     Icons.bar_chart_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.primary,
   );
 
-  static Icon homeIcon =  Icon(
+  static Icon homeIcon = Icon(
     Icons.home_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.primary,
   );
 
-  static Icon historyIcon =  Icon(
+  static Icon historyIcon = Icon(
     Icons.history_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.primary,
   );
 
-  static Icon connectIconSecondary =  Icon(
-    Icons.outlet_outlined,
+   static Icon errorIconSecondary = Icon(
+    Icons.error,
+    size: 200,
+    color: AppColors.primary,
+  );
+
+  static Icon connectIconSecondary = Icon(
+    Icons.usb,
     size: 200,
     color: AppColors.secondary,
   );
 
-  static Icon barChartIconSecondary =  Icon(
+  static Icon noConnectIconSecondary = Icon(
+    Icons.usb_off,
+    size: 200,
+    color: AppColors.secondary,
+  );
+
+  static Icon barChartIconSecondary = Icon(
     Icons.bar_chart_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.secondary,
   );
 
-  static Icon homeIconSecondary =  Icon(
+  static Icon homeIconSecondary = Icon(
     Icons.home_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.secondary,
   );
 
-  static Icon historyIconSecondary =  Icon(
+  static Icon historyIconSecondary = Icon(
     Icons.history_outlined,
     size: AppDimensions.iconLarge,
     color: AppColors.secondary,
